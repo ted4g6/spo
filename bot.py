1202770e9a3d05ffceb11b19afcf078dcb1f7dc1
ted4g6 <amerasaadali1@gmail.com>
2024-11-10 17:05:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,14 @@
+import os
 from telebot import TeleBot, types
 
-
-bot = TeleBot('7624032296:AAFMbsSLebt4iEahJQ7SxWTi9EgbxUFHrLY')
+bot = TeleBot(os.getenv('TELEGRAM_BOT_TOKEN'))
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    
     markup = types.InlineKeyboardMarkup()
     watch_button = types.InlineKeyboardButton("ابدأ ", url="http://t.me/spor7bot/sport")
     markup.add(watch_button)
     
-    
     bot.send_message(message.chat.id, "مرحباً! انقر على الزر أدناه لبدء مشاهدة المباريات:", reply_markup=markup)
-
 
 bot.polling()
